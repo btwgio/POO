ce0b8b49144960d0ed00c683cd6bb469899abd36
Giovanna <130415868+btwgio@users.noreply.github.com>
2024-02-22 20:26:15 +0000

    a

---
+++ Aula01/03.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite a nota do primeiro bimestre da disciplina: \n'))
+n2 = int(input('Digite a nota do segundo bimestre da disciplina: \n'))
+media = ((n1*2)+(n2*3))/5
+print(f'A média final é: {media}.')
